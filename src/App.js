0a6b1c8b51d9ab2fbc5a60d046e33d3dde754136
=BjChacha <bjchacha@outlook.com>
2022-06-29 16:33:58 +0800

    add: ant design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
+import { ConfigProvider, DatePicker, message, Button } from 'antd';
 import "./App.css"
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello World!</h1>
-                <h1>Hello World!</h1>
-            </div>
-        );
-    }
+
+function App() {
+    // render() 
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+            <h1>Hello World!</h1>
+            <Button onClick={() => setCount(count - 1)}>Primary</Button>
+            <Button type='primary' onClick={() => {setCount(count + 1)}}>Click me {count} times</Button>
+        </div>
+    );
+    
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { createElement } from 'react';
-// import ReactDOM, {createRoot} from 'react-dom'
+import React from 'react';
 import {createRoot} from 'react-dom/client'
 import App from './App.js'
+import 'antd/dist/antd.css'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+
 const container = document.getElementById('root');
 const root = createRoot(container)
 root.render(<App />)
