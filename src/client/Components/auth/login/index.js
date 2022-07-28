aa2894d976e7f60fd3059d198db1faf416d26aae
=BjChacha <bjchacha@outlook.com>
2022-07-28 18:25:57 +0800

    opt: login ui

--- src/client/Components/auth/login/index.js
+++ src/client/Components/auth/login/index.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 import Proptypes from 'prop-types'
 
 import './index.css'
-import { data } from 'autoprefixer';
+
+const USERNAME_REGEX = '^[a-zA-Z0-9\-]+$';
+const PASSWORD_REGEX = '^[a-zA-Z0-9!@#\$%\^&\*]+$';
 
 async function loginUser(credentials) {
-    return fetch('http://localhost:8080/login', {
+    return fetch('http://localhost:8080/auth', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -18,7 +20,7 @@ export default function Login(props) {
 
     const {setToken} = props;
 
-    const [username, setUserName] = useState();
+    const [username, setUsername] = useState();
     const [password, setPassword] = useState();
 
     const handleSubmit = async e => {
@@ -31,18 +33,29 @@ export default function Login(props) {
         setToken(token);
     };
 
+    const handleUsername = async e => {
+        e.target.value = e.target.value.replace(/![a-zA-Z0-9\-]+/, '');
+        console.log(e.target.value);
+        setUsername(e.target.value);
+    };
+    
+    const handlePassword = async e => {
+        e.target.value = e.target.value.replace(/![a-zA-Z0-9!@#\$%\^&\*]+/, '');
+        setPassword(e.target.value);
+    };
+
     return (
         <div className='login-form-container'>
             <form onSubmit={handleSubmit}>
-                <div className='login-form-item login-form-username'>
+                <div className='login-form-item'>
                     <label htmlFor="username">Username</label>
-                    <input size='12' type="text" id='username' maxLength={12} onChange={e => setUserName(e.target.value)}/>
+                    <input size='12' type="text" id='username' value={username} minLength={3} maxLength={12} placeholder={'Username (3-12)'} pattern={USERNAME_REGEX} onChange={handleUsername}/>
                 </div>
-                <div className='login-form-item login-form-password'>
+                <div className='login-form-item'>
                     <label htmlFor="password">Password</label>
-                    <input size='12' type="password" id='password' maxLength={16} onChange={e => setPassword(e.target.value)}/>
+                    <input size='12' type="password" id='password' value={password} minLength={8} maxLength={16} placeholder={'Password (8-16)'} pattern={PASSWORD_REGEX} onChange={handlePassword}/>
                 </div>
-                <div className='login-form-item login-form-button'>
+                <div className='login-form-submit'>
                     <button name='login'>Login</button>
                     <button name='register'>Register</button>
                 </div>
